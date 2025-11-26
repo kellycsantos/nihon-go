0597e81ba10a5b497276921ee2bb20be3c53d978
Kelly C Santos <kellycristinasntos@gmail.com>
2025-11-25 23:59:12 -0300

    feat: fix mock and config image

--- src/app/mocks/DestinyCard.ts
+++ src/app/mocks/DestinyCard.ts
@@ -1,4 +1,7 @@
 import tokyoImg from '../../../public/assets/tokyo.jpeg'
+import kyotoImg from '../../../public/assets/kyoto.jpeg'
+import osakaImg from '../../../public/assets/osaka.jpeg'
+
 export const DataCardDestiny = [
     {
         destiny : 'Tokyo',
@@ -8,13 +11,13 @@ export const DataCardDestiny = [
     },
     {
         destiny : 'Kyoto',
-        img: tokyoImg,
+        img: kyotoImg,
         description : "Immerse yourself in Japan's cultural heart with thousands of temples, traditional gardens, and geisha districts.",
         tags : ['Fushimi Inari', 'Golden Pavilion', 'Arashiyama']
     },
     {
         destiny : 'Osaka',
-        img: tokyoImg,
+        img: osakaImg,
         description : 'Experience the vibrant capital where cutting-edge technology meets ancient traditions in bustling neighborhoods.',
         tags : ['Osaka Castle', 'Dotonbori', 'Takoyaki']
     },
