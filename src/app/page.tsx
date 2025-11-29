8c5d71e2f05c340243de25fd263aca99ec35b59b
Kelly C Santos <kellycristinasntos@gmail.com>
2025-11-28 23:34:54 -0300

    feat: import Banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import styles from './page.module.scss'
+import { Banner } from './components';
+
 export default function Home() {
   return (
     <div className={styles.page}>
-     <h1>Nihon? Go!</h1>
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+     <Banner/>
     </div>
   );
 }
