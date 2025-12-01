424ca26ecd50033165441982cc2ba0d0354503b3
Kelly C Santos <kellycristinasntos@gmail.com>
2025-12-01 17:12:22 -0300

    styled: fix styles

--- src/app/components/EssentialCard/EssentialCard.tsx
+++ src/app/components/EssentialCard/EssentialCard.tsx
@@ -1,20 +1,18 @@
 import styles from './essentialCard.module.scss'
 import Image, { StaticImageData } from 'next/image'
 
-import trainIcon from '../../../../public/icons/train.svg'
-
 type EssentialCard = {
-    icon : StaticImageData,
+    icon: StaticImageData,
     title: string,
-    description : string,
+    description: string,
 
 }
 
 export const EssentialCard = ({icon, title, description}: EssentialCard) => {
     return (
         <div className={styles.card}>
-            <Image src={icon} alt={title} />
-            <h3>{title}</h3>
+                <Image src={icon} alt={title} />
+                <h3>{title}</h3>
             <p>{description}</p>
         </div>
     )
