152e7d3f46e58dfc2a8a851b0a81d67daa6080dc
Kelly C Santos <kellycristinasntos@gmail.com>
2025-12-01 16:48:00 -0300

    feat: add Unforgettable Experiences

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import styles from './page.module.scss'
-import { Banner, PopularDestinations } from './components';
+import { Banner, PopularDestinations, UnforgettableExperiences} from './components';
 
 export default function Home() {
   return (
     <div className={styles.page}>
      <Banner/>
      <PopularDestinations/>
+     <UnforgettableExperiences/>
     </div>
   );
 }
