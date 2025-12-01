7d212b06fadd132b1c6320794a0cd8e31d815fab
Kelly C Santos <kellycristinasntos@gmail.com>
2025-12-01 17:15:15 -0300

    feat: add Tips section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import styles from './page.module.scss'
 import { Banner, PopularDestinations } from './components';
+import { Banner, PopularDestinations, UnforgettableExperiences, Tips} from './components';
 
 export default function Home() {
   return (
     <div className={styles.page}>
      <Banner/>
      <PopularDestinations/>
+     <UnforgettableExperiences/>
+     <Tips/>
     </div>
   );
 }
